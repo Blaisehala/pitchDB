4843637dc2a7677530e998881fdc1d0dd3825284
Blaise Hala <blaisehala@gmail.com>
2022-05-09 17:22:25 +0300

    adds post list

--- pitch.py
+++ pitch.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [{'author': 'John', 'title': 'pitcher', 'content': 'pitchoned', 'date_posted':'April 1,2020'}]
+[{'author': 'Blaise Hala', 'title': 'kode', 'content': 'pitchoned2', 'date_posted':'November 1,2020'}]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 
 
