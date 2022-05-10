dbb97d7e623fe651a5a86153154a8d18309fb65d
Blaise Hala <blaisehala@gmail.com>
2022-05-10 12:06:26 +0300

    CI:initializarion of DB and addition of class User

--- pitch.py
+++ pitch.py
@@ -1,10 +1,38 @@
-from crypt import methods
+
 from flask import Flask, flash, render_template, url_for,flash,redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '89d9a808fe229b4cf4e66a3b51e52fdb'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+# database instance 
+db= SQLAlchemy(app) 
+
+
+class user(db.Model):
+  # setting unique identifer
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  username = db.Column(db.String(120), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(60),nullable=False)
+
+
+# How object is printed
+  def __repr__(self):
+      return f"User('{self.username},'{self.email}','{self.image_file}')"
+
+
+
+class Pitch(db.Model):
+
+
+
+
+
+
+
 
 
 posts =[
@@ -38,14 +66,22 @@ def register():
   form = RegistrationForm()
   if form.validate_on_submit():
     # flash message sends one time alert in flask
-    flash(f'Account created for {form.username.data}!','success')
+    flash(f'Account created for {form.username.data}!','info')
     return redirect(url_for('home'))
   return render_template ('register.html',title='Register',form=form)
 
 
-@app.route('/login')
+@app.route('/login',methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    if form.mail.data =='admin@blog.com' and  form.password.data == 'password':
+      flash('You have logged in successfully.', 'info')
+      return redirect(url_for('home'))
+    else:
+      flash('Login Unsuccessful.Please check mail or password' 'danger')
+      
+
   return render_template ('login.html',title='Login',form=form)
 
 
