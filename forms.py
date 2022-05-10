423c33f1ef8ea6318291396404e27d26cbc7d357
Blaise Hala <blaisehala@gmail.com>
2022-05-10 10:08:30 +0300

    fix:fixing variable names running errors

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class RegistrationForm (FlaskForm):
-  Username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20) ]) 
+  username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20) ]) 
   
   email = StringField('Email', validators= [DataRequired(),Email()])
 
-  pasword = PasswordField('Password', validators=[DataRequired()]) 
+  password = PasswordField('Password', validators=[DataRequired()]) 
 
   confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
